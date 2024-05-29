18bc597d3e73bf6ac2f4c7cdf33f8bc1d0ccaf97
Marek Subocz <mrksubocz@gmail.com>
2024-05-29 12:36:23 +0200

    using oil.nvim and many others

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -21,6 +21,23 @@ return {
       vim.cmd 'colorscheme gruvbox'
     end,
   },
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 
   {
     'ggandor/leap.nvim',
@@ -166,5 +183,55 @@ return {
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+  },
+  {
+    'RRethy/vim-illuminate',
+    config = function()
+      require('illuminate').configure {
+        filetypes_denylist = {
+          'TelescopePrompt',
+        },
+        modes_allowlist = { 'n', 'i' },
+      }
+      -- vim.cmd [[highlight IlluminatedWordText Search]]
+      -- vim.cmd [[highlight IlluminatedWordRead Search]]
+      -- vim.cmd [[highlight IlluminatedWordWrite Search]]
+      vim.cmd [[highlight IlluminatedWordText guibg=#504945]]
+      vim.cmd [[highlight IlluminatedWordRead guibg=#504945]]
+      vim.cmd [[highlight IlluminatedWordWrite guibg=#504945]]
+    end,
+  },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['g?'] = 'actions.show_help',
+          ['<CR>'] = 'actions.select',
+          ['<C-s>'] = 'actions.select_vsplit',
+          ['<C-h>'] = 'actions.select_split',
+          ['<C-t>'] = 'actions.select_tab',
+          ['<S-CR>'] = 'actions.preview',
+          ['<C-c>'] = 'actions.close',
+          ['<C-l>'] = 'actions.refresh',
+          ['-'] = 'actions.parent',
+          ['_'] = 'actions.open_cwd',
+          ['`'] = 'actions.cd',
+          ['~'] = 'actions.tcd',
+          ['gs'] = 'actions.change_sort',
+          ['gx'] = 'actions.open_external',
+          ['g.'] = 'actions.toggle_hidden',
+          ['g\\'] = 'actions.toggle_trash',
+        },
+        use_default_keymaps = false,
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'open parent directory' })
+    end,
   },
 }
